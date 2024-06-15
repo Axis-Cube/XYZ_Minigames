68a23d64cf2801416c580ba59ae07a24d35f9612
Dmitriy Tokarev (Lndrs_) <96104118+AbstractScripts@users.noreply.github.com>
2024-06-15 17:13:59 +0300

    Delete scripts directory

--- scripts/games/lobby.js
+++
@@ -1,9 +0,0 @@
-import { EntityInventoryComponent, system, world } from "@minecraft/server";
-import { MT_GAMES } from "../modules/MultiTasking/instances";
-export function lobbyPlateInteraction(player, block) {
-}
-export function TikTakToe() {
-    //world.afterEvents.itemUseOn.subscribe(ttt=>{
-    //    console.log(ttt.block.getItemStack().typeId, ttt.source.name)
-    //})
-}
